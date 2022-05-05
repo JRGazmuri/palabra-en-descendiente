371dcf627a28e268e24427337176b95ea48e55e8
JRGazmuri <104801689+JRGazmuri@users.noreply.github.com>
2022-05-05 18:01:59 -0400

    Add files via upload

---
+++ prueba for.py
@@ -0,0 +1,12 @@
+""" z = int(input("Ingrese hasta que numero desea que aparezca\n"))
+y = int(input("Ingrese los pasos de la iteración que desea que aparezcan\n"))
+x = range(1,z+1,y)
+for n in x:
+    print(n) """
+
+
+
+
+frase = input("ingrese frase a repetir\n")
+for char in frase:
+    print(char)
